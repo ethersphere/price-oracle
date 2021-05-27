64e2dc57dcbc03d35d706ffb01f823631a636701
Ralph Pichler <pichler.ralph@gmail.com>
2021-05-27 13:04:21 +0200

    add price functions

--- src/PriceOracle.sol
+++ src/PriceOracle.sol
@@ -2,4 +2,22 @@
 pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PriceOracle is Ownable {}
+contract PriceOracle is Ownable {
+    uint256 public price;
+    uint256 public chequeValueDeduction;
+
+    function getPrice() external view returns (uint256, uint256) {
+        return (price, chequeValueDeduction);
+    }
+
+    function updatePrice(uint256 newPrice) external onlyOwner {
+        price = newPrice;
+    }
+
+    function updateChequeValueDeduction(uint256 newChequeValueDeduction)
+        external
+        onlyOwner
+    {
+        chequeValueDeduction = newChequeValueDeduction;
+    }
+}
