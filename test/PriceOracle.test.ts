43f9724be7b4e937bab4400836c85b75c055d2b3
Ralph Pichler <pichler.ralph@gmail.com>
2021-05-27 16:33:44 +0200

    add tests and comments

--- test/PriceOracle.test.ts
+++ test/PriceOracle.test.ts
@@ -28,4 +28,72 @@ describe("PriceOracle", function () {
       expect(priceOracle.address).to.be.properAddress;
     });
   });
+
+  describe("with deployed contract", function () {
+    beforeEach(async function () {
+      await deployments.fixture();
+      this.priceOracle = await ethers.getContract("PriceOracle", deployer);
+    });
+
+    describe("when updating the price", function () {
+      describe("when called by owner", function () {
+        it("should emit the PriceUpdate event", async function () {
+          const newPrice = 100;
+          await expect(this.priceOracle.updatePrice(newPrice))
+            .to.emit(this.priceOracle, "PriceUpdate")
+            .withArgs(newPrice);
+        });
+
+        it("should update the price", async function () {
+          const newPrice = 100;
+          await this.priceOracle.updatePrice(newPrice);
+          expect(await this.priceOracle.price()).to.equal(newPrice);
+        });
+      });
+
+      describe("when not called by owner", function () {
+        it("should revert", async function () {
+          const priceOracle = await ethers.getContract(
+            "PriceOracle",
+            others[0]
+          );
+          const newPrice = 100;
+          await expect(priceOracle.updatePrice(newPrice)).to.be.revertedWith(
+            "Ownable: caller is not the owner"
+          );
+        });
+      });
+    });
+
+    describe("when updating the chequeValueDeduction", function () {
+      describe("when called by owner", function () {
+        it("should emit the ChequeValueDeductionUpdate event", async function () {
+          const newValue = 100;
+          await expect(this.priceOracle.updateChequeValueDeduction(newValue))
+            .to.emit(this.priceOracle, "ChequeValueDeductionUpdate")
+            .withArgs(newValue);
+        });
+
+        it("should update the chequeValueDeduction", async function () {
+          const newValue = 100;
+          await this.priceOracle.updateChequeValueDeduction(newValue);
+          expect(await this.priceOracle.chequeValueDeduction()).to.equal(
+            newValue
+          );
+        });
+      });
+
+      describe("when not called by owner", function () {
+        it("should revert", async function () {
+          const priceOracle = await ethers.getContract(
+            "PriceOracle",
+            others[0]
+          );
+          await expect(
+            priceOracle.updateChequeValueDeduction(100)
+          ).to.be.revertedWith("Ownable: caller is not the owner");
+        });
+      });
+    });
+  });
 });
