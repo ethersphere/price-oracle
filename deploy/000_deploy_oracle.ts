c743e971910d218f7a2d7a5534b65b293810ffa3
Marko <adriadrop@gmail.com>
2023-06-06 13:24:46 +0200

    fix deployment

--- deploy/000_deploy_oracle.ts
+++ deploy/000_deploy_oracle.ts
@@ -1,16 +1,34 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+import verify from "../utils/verify";
 
-const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre;
-  const { deploy } = deployments;
+const func: DeployFunction = async function ({
+  deployments,
+  getNamedAccounts,
+  network,
+}) {
+  const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  await deploy("PriceOracle", {
+  // This code is just used for Sepolia testnet deployment
+  const waitBlockConfirmations = network.name != "testnet" ? 1 : 6;
+
+  log("----------------------------------------------------");
+  const args = [100, 200];
+  const factory = await deploy("PriceOracle", {
     from: deployer,
-    args: [100, 200],
+    args: args,
     log: true,
+    waitConfirmations: waitBlockConfirmations,
   });
+
+  log("Factory deployed at address " + factory.address);
+
+  // Verify the deployment
+  if (network.name == "testnet" && process.env.TESTNET_ETHERSCAN_KEY) {
+    log("Verifying...");
+    await verify(factory.address, args);
+  }
 };
 
 export default func;
+module.exports.tags = ["main", "oracle"];
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,9 +3,7 @@ import { HardhatUserConfig } from "hardhat/types";
 import "solidity-coverage";
 import "hardhat-deploy";
 import "hardhat-deploy-ethers";
-import "hardhat-tracer";
 import "@nomiclabs/hardhat-etherscan";
-import "hardhat-contract-sizer";
 
 // Set Private RPCs if added, otherwise use Public that are hardcoded in this config
 
@@ -85,6 +83,12 @@ const config: HardhatUserConfig = {
         },
       },
     ],
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
   },
   paths: {
     sources: "src",
